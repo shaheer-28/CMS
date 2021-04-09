7e4d961da687ba74f36f0812aad487465bdf87d1
Shaheer Ahmad <square63@square63s-MacBook-Pro.local>
2021-04-09 15:55:24 +0500

    registrations_controller file updated as per Feedback

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
     # Override the action you want here.
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters
 
   protected
+  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :middle_name, :last_name, :country, :phone_number])
   end
