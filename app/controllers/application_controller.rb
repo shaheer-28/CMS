88b7b64a84a22e489e085e12e7d0ca1541e91e4a
Shaheer Ahmad <square63@square63s-MacBook-Pro.local>
2021-04-14 10:29:09 +0500

    Admin Sign in Done
    * Admin is able to sign in and land to a page containing list of users
    * Same sign in screen used for both normal user and admin
    * Sign out functionality also added to Admin side

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+	
+	def after_sign_in_path_for(resource)
+    if current_user.admin?
+		  stored_location_for(resource) || admin_users_path
+    elsif current_user.user?
+      stored_location_for(resource) || users_path
+    end
+	end
 end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,13 @@
+class WelcomeController < ApplicationController
+
+	before_action :check_user_logged_in
+
+	def index
+	end
+
+	private
+
+	def check_user_logged_in
+		redirect_to new_user_session_path if user_signed_in?
+	end
+end
