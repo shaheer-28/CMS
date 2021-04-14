4f699010453c36b964b1c6c8045fd77729b41ed9
Shaheer Ahmad <square63@square63s-MacBook-Pro.local>
2021-04-14 11:06:54 +0500

    User Authentication added before loading Admin pages

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < ApplicationController
-	#before_action :authenticate_user!
+	before_action :authenticate_user!
 
 	def index
 		@users = User.where(role: User.roles[:user])
