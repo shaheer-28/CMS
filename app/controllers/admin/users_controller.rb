359260ba1ae56a4a37079058a2272c76935c83e5
Shaheer Ahmad <square63@square63s-MacBook-Pro.local>
2021-04-16 16:23:07 +0500

    Update user form Admin side working

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -7,18 +7,17 @@ class Admin::UsersController < ApplicationController
     @users = User.by_role(User.roles[:user])
   end
 
-  def show 
-  end
+  def show; end
 
-  def edit
-  end
+  def edit; end
 
   def update
     @user.attributes = user_params
-    if @user.save(context: :update)
+    if @user.save(validate: false)
       redirect_to admin_users_path
+    else
+      render 'edit'
     end
-    #redirect_to admin_users_path if @user.update(first_name: params[:first_name], middle_name: params[:middle_name], last_name: params[:last_name], country: params[:country], phone_number: params[:phone_number] )
   end
 
   private
