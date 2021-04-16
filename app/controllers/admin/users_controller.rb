945bc29dac597834f8aca7533bdae3be38628054
Shaheer Ahmad <square63@square63s-MacBook-Pro.local>
2021-04-16 15:37:15 +0500

    PR Review feedback incorporated

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,14 +1,21 @@
 class Admin::UsersController < ApplicationController
+  before_action :get_user, only: %i[show update]
+
   def index
     @users = User.by_role(User.roles[:user])
   end
 
-  def show 
-    @user = User.find(params[:id])
+  def show
   end
 
   def destroy
-    @user = User.find(params[:id]).destroy
+    @user.destroy
     redirect_to admin_users_path
+  end
+
+  private
+
+  def get_user
+    @user = User.find(params[:id])
   end
 end
--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,17 +1,19 @@
 class Users::InvitationsController < Devise::InvitationsController
+  before_action :validate_admin, only: %i[create new]
+
   def new
-    if current_user.user?
-      redirect_to users_path
-    elsif current_user.admin?
-      super
-    end
+    super if current_user.admin?
   end
 
   def create
+    super if current_user.admin?
+  end
+
+  private 
+
+  def validate_admin
     if current_user.user?
       redirect_to users_path
-    elsif current_user.admin?
-      super
     end
   end
 end
