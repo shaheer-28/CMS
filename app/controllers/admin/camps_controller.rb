17d7ad6a1f9ae202442d803c4c4b4d8dc044fc8b
Shaheer Ahmad <square63@square63s-MacBook-Pro.local>
2021-04-20 15:36:01 +0500

    Admin location listing, show, edit, delete functionality implemented
    * Admin lands on Location listing page right after signing in
    * Login page design slightly updated
    * Camp model created with location as the only column

---
+++ app/controllers/admin/camps_controller.rb
@@ -0,0 +1,62 @@
+class Admin::CampsController < ApplicationController
+  before_action :check_role
+  before_action :camp_params, only: %i[update create]
+  before_action :get_camp, only: %i[show edit update destroy]
+
+  def index
+    @camps = Camp.all
+  end
+
+  def show; end
+
+  def new
+    @camp = Camp.new
+  end
+
+  def create
+    @camp = Camp.new(location: params[:camp][:location])
+    p "CAMP = #{@camp.inspect}"
+    if @camp.save
+      flash[:notice] = 'Camp has been created'
+      redirect_to admin_camp_path(@camp)
+    else
+      flash[:alert] = 'Unable to create Camp'
+      render 'new'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @camp.update(location: params[:camp][:location])
+      flash[:notice] = 'Camp has been updated'
+      redirect_to admin_camps_path
+    else
+      flash[:alert] = 'Unable to update Camp'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @camp.destroy
+      flash[:notice] = 'Camp has been deleted'
+    else
+      flash[:alert] = 'Unable to delete Camp'
+    end
+    redirect_to admin_camps_path
+  end
+
+  private
+
+  def get_camp
+    @camp = Camp.find(params[:id])
+  end
+  
+  def check_role
+    redirect_to users_path if current_user.user?
+  end
+
+  def camp_params
+    params.require(:camp).permit(:location)
+  end
+end
