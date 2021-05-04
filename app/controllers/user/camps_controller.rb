75e262b42d33defea952c15b3467ac3198885820
Shaheer Ahmad <square63@square63s-MacBook-Pro.local>
2021-05-04 15:44:46 +0500

    user/camps_controller updated

--- app/controllers/user/camps_controller.rb
+++ app/controllers/user/camps_controller.rb
@@ -10,17 +10,17 @@ class User::CampsController < User::UsersController
 
   def introduction
     @allow_to_proceed = true if @camp.end_date > Date.today
-    @camp_reg = CampsRegistration.find_by(user_id: current_user.id, camp_id: params[:id])
-    p "Camps Registrations #{@camp_reg.inspect}"
+    if @allow_to_proceed
+      @camp_reg = CampsRegistration.find_by(user_id: current_user.id, camp_id: params[:id])
 
-    if @camp_reg.present?
-      @camps_registrations = @camp_reg
-    else
-      @camps_registrations = CampsRegistration.new(user_id: current_user.id, camp_id: params[:id])
-      @camps_registrations.save
+      if @camp_reg.present?
+        @camps_registrations = @camp_reg
+      else
+        @camps_registrations = CampsRegistration.new(user_id: current_user.id, camp_id: params[:id])
+        @camps_registrations.save
+      end
+      session[:camp_reg_id] = @camps_registrations.id
     end
-    p "Camps Registrations #{@camps_registrations.inspect}"
-    session[:camp_reg_id] = @camps_registrations.id
   end
 
   private
